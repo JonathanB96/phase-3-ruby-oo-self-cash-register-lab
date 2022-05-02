3972f1cb80ad7c4ff8c400aa71f016ef8d947249
jonathan bola <bolajonathan@yahoo.com>
2022-05-02 12:29:11 -0500

    commit tree 826de26fd23924e4615d6176e7627e408e508b22

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+class CashRegister
+    attr_reader :discount
+    attr_accessor :total
+    
+    def initialize(discount)
+        @total = 0 
+        @discount = discount                
+    end    
 
+
+end
